91cc550f85b1d90ee7be2e13421c786a530e8f97
Oleksandr Husiev <ohusiev@icloud.com>
2024-09-19 22:31:41 +0200

    added balances transfer logic with tests

--- src/balances.rs
+++ src/balances.rs
@@ -1,6 +1,7 @@
 use std::collections::BTreeMap;
+
 pub struct Pallet {
-    balances: BTreeMap<String, u128>
+    balances: BTreeMap<String, u128>,
 }
 
 impl Pallet {
@@ -17,4 +18,52 @@ impl Pallet {
     pub fn balance(&self, who: &str) -> u128 {
         *self.balances.get(who).unwrap_or(&0)
     }
+
+    pub fn transfer(&mut self, from: String,
+                    to: String, amount: u128) -> Result<(), &'static str> {
+        let caller_balance = self.balance(&from);
+        let to_balance = self.balance(&to);
+        let new_caller_balance = caller_balance.checked_sub(amount)
+            .ok_or("Insufficient balance")?;
+        let new_to_balance = to_balance.checked_add(amount)
+            .ok_or("Overflow")?;
+        self.set_balance(&from, new_caller_balance);
+        self.set_balance(&to, new_to_balance);
+
+        Ok(())
+    }
+}
+
+#[test]
+fn test_balance() {
+    let mut pallet = Pallet::new();
+    const ALICE: &'static str = "Alice";
+    const ANN: &'static str = "Ann";
+    assert_eq!(pallet.balance(ALICE), 0);
+    pallet.set_balance(&ANN.to_string(), 100);
+    assert_eq!(pallet.balance(ANN), 100);
+    assert_eq!(pallet.balance(ALICE), 0);
+}
+
+#[test]
+fn transfer_balance() {
+    let mut pallet = Pallet::new();
+    const ALICE: String = "Alice".to_string();
+    const BOB: String = "Bob".to_string();
+    pallet.set_balance(&ALICE, 100);
+    pallet.set_balance(&BOB, 100);
+    pallet.transfer(ALICE, BOB, 50).unwrap();
+    assert_eq!(pallet.balance("Alice"), 50);
+    assert_eq!(pallet.balance("Bob"), 150);
+}
+
+#[test]
+fn transfer_balance_insufficient() {
+    let mut pallet = Pallet::new();
+    const ALICE: String = "Alice".to_string();
+    const BOB: String = "Bob".to_string();
+    pallet.set_balance(&ALICE, 100);
+    pallet.set_balance(&BOB, 100);
+    assert_eq!(pallet.transfer(ALICE, BOB, 150),
+               Err("Insufficient balance"));
 }
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,4 @@ mod balances;
 
 fn main() {
     println!("Hello, world!");
-    let pallet = balances::Pallet::new();
-}
-
-#[test]
-fn test_balance() {
-    let mut pallet = balances::Pallet::new();
-    assert_eq!(pallet.balance("Alice"), 0);
-    pallet.set_balance(&"Ann".to_string(), 100);
-    assert_eq!(pallet.balance("Ann"), 100);
-    assert_eq!(pallet.balance("Alice"), 0);
 }
