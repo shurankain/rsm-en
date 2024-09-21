63fbfc8a53cc265c055dbde2d11d3bb4926ffc15
Oleksandr Husiev <ohusiev@icloud.com>
2024-09-21 14:10:33 +0200

    basic fund transfer logic

--- src/main.rs
+++ src/main.rs
@@ -16,5 +16,24 @@ impl Runtime {
 }
 
 fn main() {
+    // genesis block
     let mut runtime = Runtime::new();
+
+    let alice = "Alice".to_string();
+    let bob = "Bob".to_string();
+    let charlie = "Charlie".to_string();
+
+    runtime.balances.set_balance(&alice, 100);
+    runtime.system.inc_block_number();
+    assert_eq!(runtime.system.block_number(), 1);
+    runtime.system.inc_nonce(&alice);
+
+    let _ = runtime.balances
+        .transfer(alice.clone(), bob.clone(), 50)
+        .map_err(|e| print!("Error: {:?}", e));
+
+    let _ = runtime.balances
+        .transfer(alice.clone(), charlie.clone(), 20)
+        .map_err(|e| print!("Error: {:?}", e));
+
 }
