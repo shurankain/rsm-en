33aeb4f0f6fc12ae5387bd26feaad6af3b3b6856
Oleksandr Husiev <ohusiev@icloud.com>
2024-09-21 14:01:06 +0200

    basic runtime implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 mod balances;
 mod system;
 
+pub struct Runtime {
+    balances: balances::Pallet,
+    system: system::Pallet,
+}
+
+impl Runtime {
+    pub fn new() -> Self {
+        Runtime {
+            balances: balances::Pallet::new(),
+            system: system::Pallet::new(),
+        }
+    }
+}
+
 fn main() {
+    let mut runtime = Runtime::new();
 }
