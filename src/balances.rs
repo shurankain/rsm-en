372199f9eda815830454c239b273fddff42e5d00
Oleksandr Husiev <ohusiev@icloud.com>
2024-09-29 19:32:30 +0200

    created pallet dispatch in balances.rs

--- src/balances.rs
+++ src/balances.rs
@@ -40,6 +40,24 @@ impl<T: Config> Pallet<T> {
     }
 }
 
+pub enum Call<T: Config> {
+    Transfer { to: T::AccountId, amount: T::Balance },
+}
+
+impl<T: Config> crate::support::Dispatch for Pallet<T> {
+    type Caller = T::AccountId;
+    type Call = Call<T>;
+
+    fn dispatch(&mut self, caller: Self::Caller, call: Self::Call) -> crate::support::DispatchResult {
+        match call {
+            Call::Transfer { to, amount } => {
+                self.transfer(caller, to, amount)?;
+            }
+        }
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod test {
     use crate::system;
